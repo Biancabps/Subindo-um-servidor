1ceea86f3fb6e6574cc1ae57700f221096c3c862
Bianca <bpsbianca@gmail.com>
2022-05-26 14:31:49 -0300

    feat: definindo rota HOME e CONTATO

--- app.js
+++ app.js
@@ -2,5 +2,14 @@ const http = require('http');
 
 http.createServer((req, res) => {
     res.writeHead(200, {"Content-Type":"text/plain"});
-    res.end("Meu primeiro servidor!");
+
+        switch(req.url){
+            case "/":
+                res.end("Você está na página HOME!");
+                break;
+            case "/contato":
+                res.end("Você está na página CONTATO!");
+                break;
+        }
+        
 }).listen(3030);
